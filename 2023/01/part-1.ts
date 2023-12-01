e3b76bed57048014e1b228b7a91610258e82ab1a
Tim Brand <t.brand@gmail.com>
2023-12-01 13:24:19 +0100

    feat: 2023 day 1

---
+++ 2023/01/part-1.ts
@@ -0,0 +1,9 @@
+
+const inputLines = (await Bun.file(import.meta.dir + '/part-1-input').text()).split('\n')
+let sum = 0
+inputLines.forEach((line, i) => {
+    const digits = line.replaceAll(/[a-z]*/g, "")
+    sum += +`${digits[0]}${digits.at(-1)}`
+})
+
+console.log(sum)
