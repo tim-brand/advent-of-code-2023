f63c8b5e7f3a667633c73388d5e744c9fcc2ad31
Tim Brand <t.brand@gmail.com>
2023-12-02 21:01:59 +0100

    chore: refactor

---
+++ days/01/index.ts
@@ -0,0 +1,41 @@
+const parseInput = (input: string): string[] => {
+    return input.split('\n')
+};
+
+export const part1 = (input: string): number => {
+    let sum = 0
+    parseInput(input).forEach((line, i) => {
+        const digits = line.replaceAll(/[a-z]*/g, "")
+        sum += +`${digits[0]}${digits.at(-1)}`
+    })
+    return sum;
+};
+
+export const part2 = (input: string): number => {
+    const numberMap: Record<string, string> = {
+        'one': "1",
+        'two': "2",
+        'three': "3",
+        'four': "4", 
+        'five': "5",
+        'six': "6",
+        'seven': "7",
+        'eight': "8",
+        'nine': "9"
+    }
+    
+    let sum = 0
+    parseInput(input).forEach((line, i) => {
+        let updatedLine = line
+        for (let i = 0; i < line.length; ++i) {
+            const key = Object.keys(numberMap).find(key => line.indexOf(key, i) == i)
+            if (!key) continue;
+            updatedLine = updatedLine.replace(key, `${key[0]}${numberMap[key]}${key.at(-1)}`)
+        }
+        const digits = updatedLine.replaceAll(/[a-z\s]*/g, "")
+        const value = +`${digits[0]}${digits.at(-1)}`
+        sum += value
+    })
+    
+    return sum;
+};
--- days/01/part-1.ts
+++
@@ -1,9 +0,0 @@
-
-const inputLines = (await Bun.file(import.meta.dir + '/input').text()).split('\n')
-let sum = 0
-inputLines.forEach((line, i) => {
-    const digits = line.replaceAll(/[a-z]*/g, "")
-    sum += +`${digits[0]}${digits.at(-1)}`
-})
-
-console.log(sum)
