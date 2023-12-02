b3f7f3e594f8e7e4181fb51c197e632253a7f402
Tim Brand <t.brand@gmail.com>
2023-12-02 23:25:46 +0100

    feat(day02): finished day-02

---
+++ days/02/index.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "bun:test";
+
+import { part1, part2 } from ".";
+
+describe("day 02", () => {
+    describe("part 1", () => {
+        it("should work with the sample input", () => {
+            const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+            expect(part1(input)).toBe(8);
+        });
+    });
+
+    describe("part 2", () => {
+        it("should work with the sample input", () => {
+            const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+            expect(part2(input)).toBe(2286);
+        });
+    });
+});
---
+++ days/02/index.ts
@@ -0,0 +1,52 @@
+// DAY: 02
+
+const parseInput = (input: string): string[] => {
+    return input.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+};
+
+export const part1 = (input: string): number => {
+    let result = 0
+    parseInput(input).forEach((line, i) => {
+        const matchId = parseInt(line.match(/Game (\d+):/i)?.[1] ?? "-1")
+        const cubeShows = line.split(";")
+        const bagLimits: Record<string, number> = {
+            red: 12,
+            green: 13,
+            blue: 14
+        }
+        let bagIsValid = true;
+        for (const cubeShow of cubeShows) {
+            const cubeSets = cubeShow.matchAll(/\s(?<count>\d+)\s(?<color>[blue|green|red]+)/gi)
+            for (const cubeSet of cubeSets) {
+                const [_, count, color] = cubeSet
+                bagIsValid = parseInt(count) > bagLimits[color] ? false : bagIsValid;
+            }
+        };
+        result += bagIsValid ? matchId : 0;
+    })
+
+    return result;
+};
+
+export const part2 = (input: string): number => {
+    let result = 0
+    parseInput(input).forEach((line, i) => {
+        const cubeShows = line.split(";")
+        const bagFewedCubes: Record<string, number> = {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+
+        for (const cubeShow of cubeShows) {
+            const cubeSets = cubeShow.matchAll(/\s(?<count>\d+)\s(?<color>[blue|green|red]+)/gi)
+            for (const cubeSet of cubeSets) {
+                const [_, count, color] = cubeSet
+                bagFewedCubes[color] = Math.max(parseInt(count), bagFewedCubes[color])
+            }
+        }
+        result += bagFewedCubes.red * bagFewedCubes.green * bagFewedCubes.blue;
+    })
+    
+    return result;
+};
